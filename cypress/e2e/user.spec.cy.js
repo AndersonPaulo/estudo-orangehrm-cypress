08f8824cf3702ac0be79305ca8edf555576829d0
Anderson Paulo <andersonpauloti@gmail.com>
2025-02-10 14:07:44 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,7 +10,14 @@ describe('Orange HRM Tests ', () => {
     sectionTitleTopBar:".oxd-layout-navigation",
     dashboardGrid:".orangehrm-dashboard-grid",
     wrongCredentialAlert:".oxd-alert",
-    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']"
+    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField:"[name='firstName']",
+    middleNameField:"[name='middleName']",
+    lastNameField:"[name='lastName']",
+    genericField:".oxd-input",
+    dateField:"[placeholder='yyyy-dd-mm']",
+    dateCloseButton:".--close",
+    submitbutton:"[type='submit']"
   }
 
  
@@ -23,6 +30,19 @@ describe('Orange HRM Tests ', () => {
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.middleNameField).clear().type('middleNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('lastNameTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('555252')
+    cy.get(selectorsList.genericField).eq(5).clear().type('231456')
+    cy.get(selectorsList.genericField).eq(6).clear().type('ABFS5252')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-21-08')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type('1988-21-08')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitbutton).eq(0).click()
+    cy.get('body').should('contain','Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
